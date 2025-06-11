63fd6c00851184224d3edd43c90f60136df6e0ad
jimao <shihao.gu@perfma.com>
2025-06-11 10:46:22 +0800

    chore: add middlware

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_proxy_middleware_1 = require("http-proxy-middleware");
 const app = (0, express_1.default)();
+app.use('/api', (req, res, next) => {
+    console.log(req.url, req.body);
+    req.headers.cookie = 'sys_token=13e2ad0f65574cc497759e121573de67; sys_env_id=822313712173449216; sys_env_code=Init';
+    next();
+});
 const proxyMiddleware = (0, http_proxy_middleware_1.createProxyMiddleware)({
     target: 'http://10.10.30.103:8081/api',
     changeOrigin: true,
--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import type { NextFunction, Request, Response } from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import type { Filter, Options, RequestHandler } from 'http-proxy-middleware';
 
 const app = express();
+
+app.use('/api', (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.body);
+  req.headers.cookie = 'sys_token=13e2ad0f65574cc497759e121573de67; sys_env_id=822313712173449216; sys_env_code=Init';
+  next();
+});
 
 const proxyMiddleware = createProxyMiddleware<Request, Response>({
   target: 'http://10.10.30.103:8081/api',
