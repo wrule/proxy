9e1dc129a537af27ee08cb6735b5db74f89adcee
jimao <shihao.gu@perfma.com>
2025-06-11 10:21:59 +0800

    chore: copy index.ts

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log(1234);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const app = (0, express_1.default)();
+const proxyMiddleware = (0, http_proxy_middleware_1.createProxyMiddleware)({
+    target: 'http://www.example.org/api',
+    changeOrigin: true,
+});
+app.use('/api', proxyMiddleware);
+app.listen(3000);
--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
+import express from 'express';
+import type { NextFunction, Request, Response } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import type { Filter, Options, RequestHandler } from 'http-proxy-middleware';
 
-console.log(1234);
+const app = express();
+
+const proxyMiddleware = createProxyMiddleware<Request, Response>({
+  target: 'http://www.example.org/api',
+  changeOrigin: true,
+});
+
+app.use('/api', proxyMiddleware);
+
+app.listen(3000);
